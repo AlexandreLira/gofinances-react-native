213586e137ac18721e4c00bf223561a851dfc010
Alexandre Lira <alexandrelira112@gmail.com>
2022-09-01 19:04:25 -0300

    first commit

---
+++ src/components/Welcome.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Text, View } from "react-native";
+
+
+interface Props {
+    title: string;
+}
+
+export function Welcome({ title }: Props) {
+    return (
+        <View>
+            <Text>{title}</Text>
+        </View>
+    )
+}
