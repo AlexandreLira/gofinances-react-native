1a4659173cfdfd19c0db74a6f987cbadc25276c2
Alexandre Lira <alexandrelira112@gmail.com>
2022-09-02 06:21:30 -0300

    profile header

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,9 +1,36 @@
-import { Container, Title } from "./styles";
+import React from "react";
+import {
+    Container,
+    Header,
+    Photo,
+    PowerIcon,
+    User,
+    UserGreeting,
+    UserInfo,
+    UserName,
+    UserWrapper,
+} from "./styles";
 
-export function Dashboard(){
+export function Dashboard() {
     return (
         <Container>
-            <Title>Dashboard</Title>
+            <Header>
+                <UserWrapper>
+
+                    <UserInfo>
+                        <Photo
+                            source={{ uri: 'https://avatars.githubusercontent.com/u/58709086?v=4' }}
+                        />
+                        <User>
+                            <UserGreeting>Olá,</UserGreeting>
+                            <UserName>Alexandre</UserName>
+                        </User>
+                    </UserInfo>
+
+                    <PowerIcon name="power"/>
+                    
+                </UserWrapper>
+            </Header>
         </Container>
     )
 }
