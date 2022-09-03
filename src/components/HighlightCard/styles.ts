696721ee0455454eb418e6c8cb1d740ea8df1a34
Alexandre Lira <alexandrelira112@gmail.com>
2022-09-03 12:17:47 -0300

    typing the HighlighCard

--- src/components/HighlightCard/styles.ts
+++ src/components/HighlightCard/styles.ts
@@ -1,10 +1,14 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 import { Feather } from '@expo/vector-icons';
 import { RFValue } from 'react-native-responsive-fontsize';
+import { HighlightCardProps } from '.';
 
+type TypeProps = Pick<HighlightCardProps, "type">
 
-export const Container = styled.View`
-    background-color: ${({ theme }) => theme.colors.shape};
+export const Container = styled.View<TypeProps>`
+    background-color: ${({ theme: { colors }, type }) => (
+        type === 'total' ? colors.secondary : colors.shape
+    )};
     width: ${RFValue(300)}px;
     border-radius: 5px;
 
@@ -18,31 +22,47 @@ export const Header = styled.View`
     justify-content: space-between;
 `;
 
-export const Title = styled.Text`
+export const Title = styled.Text<TypeProps>`
     font-family: ${({ theme }) => theme.fonts.regular};
     font-size: ${RFValue(14)}px;
-    color: ${({ theme }) => theme.colors.title};
+    color: ${({ theme: { colors }, type }) => (
+        type === 'total' ? colors.shape : colors.title
+    )};
 `;
 
-export const Icon = styled(Feather)`
+export const Icon = styled(Feather) <TypeProps>`
     font-size: ${RFValue(40)}px;
-    color: ${({ theme }) => theme.colors.success};
+
+    ${({ type }) => type === 'up' && css`
+        color: ${({ theme }) => theme.colors.success};
+    `}
+
+    ${({ type }) => type === 'down' && css`
+        color: ${({ theme }) => theme.colors.attention};
+    `}
+
+    ${({ type }) => type === 'total' && css`
+        color: ${({ theme }) => theme.colors.shape};
+    `}
 `;
 
 export const Footer = styled.View`
 
 `;
 
-export const Amount = styled.Text`
+export const Amount = styled.Text<TypeProps>`
     font-family: ${({ theme }) => theme.fonts.medium};
     font-size: ${RFValue(32)}px;
-    color: ${({ theme }) => theme.colors.title};
+    color: ${({ theme: { colors }, type }) => (
+        type === 'total' ? colors.shape : colors.title
+    )};
     margin-top: 38px;
 `;
 
-export const LastTransaction = styled.Text`
+export const LastTransaction = styled.Text<TypeProps>`
     font-family: ${({ theme }) => theme.fonts.regular};
     font-size: ${RFValue(12)}px;
-
-    color: ${({ theme }) => theme.colors.text};
+    color: ${({ theme: {colors}, type }) => (
+        type === 'total' ? colors.shape : colors.text
+    )};
 `;
--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { HighlightCard } from "../../components/HighlightCard";
+import { HighlightCard, HighlightCardProps } from "../../components/HighlightCard";
 import {
     Container,
     Header,
@@ -14,6 +14,28 @@ import {
 } from "./styles";
 
 export function Dashboard() {
+
+    const data: HighlightCardProps[] = [
+        {
+            title: 'Entradas',
+            amount: 'R$ 17.400,00',
+            lastTransaction: 'Últimas entradas dia 13 de abril',
+            type: 'up'
+        },
+        {
+            title: 'Saídas',
+            amount: 'R$ 1.259,00',
+            lastTransaction: 'Últimas entradas dia 13 de abril',
+            type: 'down',
+        },
+        {
+            title: 'Total',
+            amount: 'R$ 16.141,00',
+            lastTransaction: '01 à 16 de abril',
+            type: 'total'
+        }
+    ]
+
     return (
         <Container>
             <Header>
@@ -35,9 +57,15 @@ export function Dashboard() {
             </Header>
 
             <HighlightCards>
-                <HighlightCard/> 
-                <HighlightCard/> 
-                <HighlightCard/> 
+                {data.map(item => (
+                    <HighlightCard 
+                        key={item.title}
+                        type={item.type}
+                        title={item.title}
+                        amount={item.amount}
+                        lastTransaction={item.lastTransaction}
+                    />  
+                ))}
             </HighlightCards>
         </Container>
     )
